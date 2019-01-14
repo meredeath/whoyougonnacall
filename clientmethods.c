579acc5917123cec3eb5c43af963f019d3b7bcd7
Milan Haiman <mhaiman@stuy.edu>
2019-01-14 17:46:06 -0500

    client intro method (not tested yet)

--- clientmethods.c
+++ clientmethods.c
@@ -5,20 +5,25 @@
 #define clear printf("\033[H\033[J")
 
 int intro() { // prints stuff
-	clear;
-	printf("Welcome to GHOST!\n");
-  	printf("Here are your options:\n");
-  	printf("\n");
-  	printf("1. Read Instructions\n");
-  	printf("2. Join Game\n");
-  	printf("\n");
-  	printf("Choose an option (1 or 2): ");
   	char input[16];
-  	fgets(input, 16, stdin);
-  	if(!strncmp(input,"1",1)) {
-  		
-  	}
-
+  	while(1) {
+  		clear;
+		printf("Welcome to GHOST!\nHere are your options:\n\n[1] Read Instructions\n[2] Join Game\n\nChoose an option (1 or 2): ");
+		fgets(input, 16, stdin);
+	  	if(!strncmp(input,"1",1)) {
+	  		clear;
+	  		printf("Insert Instructions Here\n", ); // What do we want to put here?
+	  		printf("Press Enter to return to the main menu.");
+	  		fgets(input, 16, stdin);
+	  		continue; // goes back to main menu
+	  	}
+	  	if(!strncmp(input,"2",1)) {
+	  		break;
+	  		// should now go to wait after intro() is done
+	  	}
+	  	clear;
+	  	printf("░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄\n░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█\n░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░█\n░▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░█\n█▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒█\n█▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█\n░█▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█\n░░█░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█\n░░░█░░██░░▀█▄▄▄█▄▄█▄████░█\n░░░░█░░░▀▀▄░█░░░█░███████░█\n░░░░░▀▄░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█\n░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░█\n░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░█\n░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░█\nPlease follow directions");
+	}
 }
 
 char * waiting(int server_socket); //block until receive message, return the message
