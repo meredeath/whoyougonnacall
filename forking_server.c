fe975d58ca6771c079afffe2545b921f9d2c8c3a
tfabiha <tfabiha@stuy.edu>
2019-01-13 21:48:14 -0500

    testing network

---
+++ forking_server.c
@@ -0,0 +1,63 @@
+#include "networking.h"
+
+void process(char *s);
+void subserver(int from_client);
+
+int main() {
+
+  int listen_socket;
+  int f;
+  listen_socket = server_setup();
+
+  int num_players = 2;
+  
+  int players[num_players];
+  char buffer[BUFFER_SIZE];
+  
+  for (int i = 0; i < num_players; i++)
+    {
+      players[i] = server_connect(listen_socket);
+
+      sprintf(buffer, "Welcome Player %d! Please patiently wait for all other players to connect.\n", i);
+      
+      write( players[i], buffer, sizeof(buffer) );
+      // if ( read(players[i], buffer, ) )
+    }
+
+  for (int i = 0; i < num_players; i++)
+    {
+      sprintf(buffer, "All players have connected!\n");
+      
+      write( players[i], buffer, sizeof(buffer) );
+    }
+
+  int current_player = 0;
+  sprintf(buffer, " ");
+    
+  while (1)
+    {
+      if (strcmp(buffer, " ") == 0)
+	{
+	  sprintf(buffer, "Congrats! You are the first player!\n");
+	  
+	  write( players[current_player], buffer, sizeof(buffer) );
+	}
+      else
+	{
+	  int prev_player = current_player - 1;
+
+	  if (prev_player < 0)
+	    prev_player = num_players - 1;
+
+	  read( players[prev_player], buffer, sizeof(buffer) );
+
+	  sprintf(buffer, "This is what the previous player has said: %s\n", buffer);
+
+	  write( players[current_player], buffer, sizeof(buffer) );
+	}
+	
+      sprintf(buffer, "Please say something to the next player: ");
+      
+      write( players[current_player], buffer, sizeof(buffer) );
+    }
+}
